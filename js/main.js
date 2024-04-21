f243f35ab048050f5e56dc7ccebdb1b9bd660796
eggirr <vlad.grishanenko.g@mail.ru>
2024-04-21 20:23:04 +0700

    Practice work 6.8

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
+        brand: 'Vue Mastery',
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
@@ -15,15 +16,18 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+        selectedVariant: 0,
     },
     methods: {
         addToCart() {
@@ -32,8 +36,17 @@ let app = new Vue({
         deleteFromCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
         }
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        }
+    }
 })

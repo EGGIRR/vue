8e4c2e5fa4d069631d8e3a0d20002bf6dcb13915
eggirr <vlad.grishanenko.g@mail.ru>
2024-04-22 19:05:44 +0700

    Practice work 10

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ Vue.component('product', {
     template: `
 <div class="product">
 <div class="product-image">
-            <img :src="image" :alt="altText"/>
-        </div>
+<img :src="image" :alt="altText"/>
+</div>
         <div class="product-info">
             <h1>{{ title }}</h1>
             <p>{{ description }}</p>
@@ -36,9 +36,6 @@ Vue.component('product', {
             <ul v-for="size in sizes">
                 <li>{{ size }}</li>
             </ul>
-            <div class="cart">
-                <p>Cart({{ cart }})</p>
-            </div>
             <button
                     v-on:click="addToCart"
                     :disabled="!inStock"
@@ -48,6 +45,19 @@ Vue.component('product', {
             </button>
             <button v-on:click="deleteFromCart">Delete from cart</button>
         </div>
+        <product-review @review-submitted="addReview"></product-review>
+           <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                  <li v-for="review in reviews">
+                  <p>{{ review.name }}</p>
+                  <p>Rating: {{ review.rating }}</p>
+                  <p>{{ review.review }}</p>
+                  <p>Recommend: {{ review.choice }}</p>
+                  </li>
+                </ul>
+            </div>
 </div>
 `,
     data() {
@@ -77,6 +87,7 @@ Vue.component('product', {
             ],
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
             cart: 0,
+            reviews: [],
             selectedVariant: 0,
         }
     },
@@ -92,6 +103,9 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         },
     },
     computed: {
@@ -116,6 +130,85 @@ Vue.component('product', {
         },
     }
 })
+
+Vue.component('product-review', {
+    template: `
+<form class="review-form" @submit.prevent="onSubmit">
+<p v-if="errors.length">
+<b>Please correct the following error(s):</b>
+<ul>
+<li v-for="error in errors">{{ error }}</li>
+</ul>
+</p>
+<p>
+<label for="name">Name:</label>
+<input id="name" v-model="name" placeholder="name">
+</p>
+<p>
+<label for="review">Review:</label>
+<textarea id="review" v-model="review"></textarea>
+</p>
+<p>
+<label for="rating">Rating:</label>
+<select id="rating" v-model.number="rating">
+<option>5</option>
+<option>4</option>
+<option>3</option>
+<option>2</option>
+<option>1</option>
+</select>
+</p>
+<p>
+<label for="choice">Would you recommend this product?:</label>
+    <div>
+    <input type="radio" id="yes" name="choice" v-model="choice" value="yes"/>
+    <label for="yes">yes</label>
+    </div>
+    <div>
+    <input type="radio" id="no" name="choice" v-model="choice" value="no" />
+    <label for="no">no</label>
+    </div>
+</p>
+<p>
+<input type="submit" value="Submit">
+</p>
+</form>
+`,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            choice: null,
+            errors: []
+
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.name && this.review && this.rating && this.choice) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    choice: this.choice
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.choice = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.choice) this.errors.push("Choice required.")
+            }
+
+        }
+    }
+})
+
 
 let app = new Vue({
     el: '#app',

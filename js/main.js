23e257fdd7a72b9358ae039de84f6e9e8d59804d
gr423_grvev <vlad.grishanenko.g@mail.ru>
2024-04-22 09:09:30 +0700

    Practice work 7

--- js/main.js
+++ js/main.js
@@ -4,25 +4,24 @@ let app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
         brand: 'Vue Mastery',
-        image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
         inventory: 100,
-        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
+                variantQuantity: 10,
+                onSale: true,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
+                variantQuantity: 0,
+                onSale: false,
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -40,13 +39,19 @@ let app = new Vue({
             this.selectedVariant = index;
             console.log(index);
         },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.variants[this.selectedVariant].onSale
         }
     }
 })
